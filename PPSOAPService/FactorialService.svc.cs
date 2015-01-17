42a639b22b9c96832f6da8135b03acb5e6009c12
worst-nick-ever <firelord@abv.bg>
2015-01-17 22:43:44 +0100

    init
    initial project commit

---
+++ PPSOAPService/FactorialService.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PPSOAPService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FactorialService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select FactorialService.svc or FactorialService.svc.cs at the Solution Explorer and start debugging.
+    public class FactorialService : IFactorialService
+    {
+        public CalculateFactorialResponse CalculateFactorial(uint number)
+        {
+            CalculateFactorialResponse response = new CalculateFactorialResponse();
+
+            if (number <= 0)
+            {
+                response.Status = -1;
+                response.Value = 0;
+                response.Message = "Invalid input";
+
+                return response;
+            }
+
+            for (uint i = 1; i <= number; i++)
+            {
+                response.Value *= i;
+            }
+
+            response.Status = 0;
+            response.Message = "Success!";
+
+            return response;
+        }
+    }
+}
---
+++ PPSOAPService/IFactorialService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PPSOAPService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFactorialService" in both code and config file together.
+    [ServiceContract]
+    public interface IFactorialService
+    {
+        [OperationContract]
+        CalculateFactorialResponse CalculateFactorial(uint number);
+    }
+
+    [DataContract]
+    public class CalculateFactorialResponse
+    {
+        private int status;
+        private double factorial = 1;
+        private string message;
+
+        [DataMember]
+        public int Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        [DataMember]
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        [DataMember]
+        public double Value
+        {
+            get { return factorial; }
+            set { factorial = value; }
+        }
+    }
+}
